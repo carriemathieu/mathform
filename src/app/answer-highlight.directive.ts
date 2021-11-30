af6d056f486f728b20c180de3b2a069c942cf61c
Carrie Mathieu <carrieburks@gmail.com>
2021-11-30 17:53:21 -0500

    answerhighlight completed

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { map } from 'rxjs/operators'
 
 @Directive({
   selector: '[appAnswerHighlight]'
@@ -8,6 +9,19 @@ export class AnswerHighlightDirective {
 
   constructor(private el: ElementRef, private controlName: NgControl) { 
 
+  }
+
+  ngOnInit() {
+    this.controlName.control?.parent?.valueChanges.pipe(
+      // determines how far answer is from correct answer. Math.abs will turn neg into positive if needed
+      map(({ a, b, answer }) => Math.abs((a + b - answer)/(a + b)))
+    ).subscribe(value => {
+      if (value < 0.2) {
+        this.el.nativeElement.classList.add('close')
+      } else {
+        this.el.nativeElement.classList.remove('close')
+      }
+    })
   }
 
 }
