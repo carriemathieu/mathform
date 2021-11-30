f4787f5297cad7890be3f7ab57655bac58f82280
Carrie Mathieu <carrieburks@gmail.com>
2021-11-30 17:40:28 -0500

    math validator, create answer-highlight directive

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-
+import { delay, filter, scan } from 'rxjs/operators'
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -11,6 +11,8 @@ import { MathValidators } from '../math-validators';
 
 // adding [(form: abstractcontrol) => {}] - custom validator for *entire* form
 export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
+
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
@@ -32,6 +34,35 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // status changes emits a value saying whether or not form is valid
+    this.mathForm.statusChanges.pipe(
+      // only continue rest of pipe if value is valid
+      filter(value => value === 'VALID'),
+      // allows answer to be shown before resetting form (100ms)
+      delay(100),
+      scan((acc) => {
+        return {
+          // adds 1 to number solved
+          numberSolved: acc.numberSolved + 1,
+          startTime: acc.startTime
+        }
+      }, {numberSolved: 0, startTime: new Date() })
+      ).subscribe(({ numberSolved, startTime }) => {
+        this.secondsPerSolution = (
+          // gives ms value -- total elapsed time
+          new Date().getTime() - startTime.getTime()
+        ) / numberSolved / 1000; // divide by 1000 to convert to ms 
+
+        // this.mathForm.controls.a.setValue(this.randomNumber())
+        // this.mathForm.controls.b.setValue(this.randomNumber())
+        // this.mathForm.controls.answer.setValue('')
+        // setValue = UPDATE ALL FORM VALUES, patchValue = UPDATE ONLY PART OF FORM
+        this.mathForm.setValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: ''
+      })
+    })
   }
 
   randomNumber() {
